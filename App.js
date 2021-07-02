0caaad9b4cc92fe7788dc5cfa97ab67a917f4f49
Musab-Al-omari <musabsomari@gmail.com>
2021-07-02 18:37:30 +0300

    todo75%

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { StyleSheet, TextInput, Text, View, KeyboardAvoidingView, Platform, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Tasks';
 export default function App() {
+
+
+  const [task, setTask] = useState()
+  const [taskItems, setTaskItem] = useState([])
+
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItem([...taskItems, task])
+    setTask('')
+  }
+
+
+  const completeTask = (index) => {
+    let itemCopy = [...taskItems];
+    itemCopy.splice(index, 1);
+    setTaskItem(itemCopy);
+  }
   return (
+
     <View style={styles.container}>
 
       {/* my todo application */}
       <View style={styles.tasksWrapper}>
-
         <Text style={styles.sectionTitle}>LIST TO DO</Text>
-
         <View style={styles.items}>
-             <Task text={'mosab'}/>
-             <Task text={'ali'}/>
-        </View>
 
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={()=>completeTask(index)}>
+                  <Task  text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
       </View>
+
+
+      {/* write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput clearButtonMode="always" style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper} >
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
 
     </View>
   );
@@ -27,19 +67,50 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E8EAED',
   },
-  tasksWrapper:{
-    paddingTop:80,
-    paddingHorizontal:20,
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
 
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold',
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
 
   },
-  items:{
-    marginTop:30,
-  }
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: "center"
+  },
+  input: {
+    paddingVertical: 15,
+    width: 250,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
 
+
+  },
+  addText: {
+
+  },
 
 });
